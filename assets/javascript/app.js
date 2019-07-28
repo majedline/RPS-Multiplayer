2e749d03ee8fa32a88f164682d98d023476df211
majedline <majed.atwi@gmail.com>
2019-07-28 11:42:33 -0400

    Added Game and player class. Created a connection to firebase and tested the connection

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,1 +1,40 @@
-//code
+/*
+Only two users can play at the same time.
+Both players pick either rock, paper or scissors. After the players make their selection, the game will tell them whether a tie occurred or if one player defeated the other.
+The game will track each player's wins and losses.
+Throw some chat functionality in there! No online multiplayer game is complete without having to endure endless taunts and insults from your jerk opponent.
+Styling and theme are completely up to you. Get Creative!
+Deploy your assignment to Github Pages.
+*/
+
+/******************OBJECTS********************** */
+function Player(name, id) {
+    this.name = name;
+    this.id = id;
+    this.score = 0;
+    this.myTrun = false;
+}
+
+function Game(id, player1, player2) {
+    this.chat = "";
+    this.id = id;
+    this.player1 = player1;
+    this.player2 = player2;
+}
+
+Game.prototype.addChat = function (playerName, text) {
+    this.chat += "<br/> [" + playerName + "] " + text;
+}
+
+/*************************************************** */
+
+var thisGame;
+var playerOne;
+var playerTwo;
+
+var game = new Game("1", new Player("jim", 1), new Player("Jow", 2));
+
+database.ref().set({
+    Game: game
+});
+
---
+++ assets/javascript/connection.js
@@ -0,0 +1,16 @@
+ // Your web app's Firebase configuration
+ const firebaseConfig = {
+    apiKey: "AIzaSyB1Fadv4nhZxGtIMn17wwtemfUfMBMPhNA",
+    authDomain: "rps-multiplayer-ca25e.firebaseapp.com",
+    databaseURL: "https://rps-multiplayer-ca25e.firebaseio.com",
+    projectId: "rps-multiplayer-ca25e",
+    storageBucket: "",
+    messagingSenderId: "659969455293",
+    appId: "1:659969455293:web:e6164a145c257521"
+  };
+
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+  var database = firebase.database();
+  
